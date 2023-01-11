2b61d8ff6a21227a6b0db98be44c932d8bce65b0
gabrielmendes98 <gabrielmssantiago@gmail.com>
2023-01-10 23:35:13 -0300

    usando banco de dados mysql para persistir dados do servidor node + nginx

--- desafio-node-nginx/nodejs/index.js
+++ desafio-node-nginx/nodejs/index.js
@@ -1,21 +1,31 @@
 const express = require("express");
 const app = express();
 const port = 3000;
-// const config = {
-//   host: "db",
-//   user: "root",
-//   password: "root",
-//   database: "nodedb",
-// };
-// const mysql = require("mysql");
-// const connection = mysql.createConnection(config);
-
-// const sql = `INSERT INTO people(name) values('Wesley')`;
-// connection.query(sql);
-// connection.end();
+const config = {
+  host: "db",
+  user: "root",
+  password: "root",
+  database: "nodedb",
+};
+const { faker } = require("@faker-js/faker");
+const mysql = require("mysql");
+const connection = mysql.createConnection(config);
+const createTable = `CREATE TABLE IF NOT EXISTS people (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255))`;
+connection.query(createTable);
+connection.end();
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  const name = faker.name.fullName();
+  const connection = mysql.createConnection(config);
+  const insertName = `INSERT INTO people(name) values('${name}')`;
+  connection.query(insertName);
+  const getNames = `SELECT * FROM people`;
+  connection.query(getNames, (error, results) => {
+    const names = results.map((result) => result.name);
+    const items = names.map((name) => `<li>${name}</li>`).join("");
+    res.send(`<h1>Full Cycle Rocks!</h1><ul>${items}</ul>`);
+  });
+  connection.end();
 });
 
 app.listen(port, () => {
